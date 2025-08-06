37537e98c3c422e15cf1a7074f932edbc50a8efa
Rainatou1 <rainatouboubacars@gmail.com>
2025-08-06 16:54:33 +0200

    importation de Image pour page produit

--- src/app/produit/page.js
+++ src/app/produit/page.js
@@ -1,5 +1,6 @@
 "use client";
 import Link from 'next/link'
+import Image from "next/image";
 
 import React, { useState } from 'react';
 import { Search, Filter, Star, ShoppingCart, Eye, Heart, ChevronDown } from 'lucide-react';
